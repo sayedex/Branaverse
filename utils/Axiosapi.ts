0d8d711d368bc5ce5e7e8d523e14db73f0877950
ame <sayedadmod@gmail.com>
2023-04-16 22:18:44 +0600

    last

--- utils/Axiosapi.ts
+++ utils/Axiosapi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //http://31.220.53.102:5000/
 //http://localhost:5000/api/
 const newRequest = axios.create({
-  baseURL: "http://31.220.53.102:5000/api",
+  baseURL: "https://api.branaverse.com/",
   withCredentials: true,
 });
 
